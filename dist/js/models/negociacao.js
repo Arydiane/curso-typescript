1dd73a08a1d0e17ae24ce4ff437c6573923875d4
Arydiane <arydianejardim@gmail.com>
2023-01-09 16:41:01 -0300

    Adiciona visualização das negociações

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -5,7 +5,7 @@ export class Negociacao {
         this.valor = valor;
     }
     get data() {
-        const data = new Date(this.data.getTime());
+        const data = new Date(this._data.getTime());
         return data;
     }
     get volume() {
