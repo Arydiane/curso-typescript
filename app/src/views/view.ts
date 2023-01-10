118523e3e43201f2b17fb1798c9d654edfaa3683
Arydiane <arydianejardim@gmail.com>
2023-01-10 20:07:34 -0300

    Adiciona o decorator dom-injector

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -16,8 +16,6 @@ export abstract class View<T>{
 
     protected abstract template(model: T): string;
     
-    @logarTempoExecucao(true)
-    @inspect
     public update(model: T): void {
         let template = this.template(model); 
         this.elemento.innerHTML = template
